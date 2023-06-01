14792634c567b9f0a7735fe9eb89954a7cc283b8
RikkeAnkerstjerne <rikke.ankerstjerne@hotmail.com>
2023-06-01 14:18:28 +0200

    added platformer scene

--- BaseGameMap/Assets/path.cs
+++ BaseGameMap/Assets/path.cs
@@ -1,24 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class path : MonoBehaviour
 {
-     
-     [SerializeField] Transform [] Points;
-     [SerializeField] private float moveSpeed;
-     [SerializeField] private float distanceToNearestPointThreshold = 0.02f;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float distanceToNearestPointThreshold = 0.02f;
 
-     private int pointsIndex;
-
-     Vector2 targetPosition = Vector2.zero;
+    Vector2 targetPosition = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        //transform.position = Points[pointsIndex].transform.position;
-        Debug.Log("Current index: "+pointsIndex);
-
     }
 
     // Update is called once per frame
@@ -28,18 +22,7 @@ public class path : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
         }
-        // if(pointsIndex <= Points.Length -1)
-        // {
-        //     var targetPosition = Points[pointsIndex].transform.position;
-        //     transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
 
-        //     // if(transform.position == Points[pointsIndex].transform.position)
-        //     if (Vector2.Distance(transform.position, Points[pointsIndex].transform.position) < distanceToNearestPointThreshold)
-        //     {
-        //         pointsIndex += 1;
-        //         Debug.Log("Current index: "+pointsIndex);
-        //     }
-        // }
         if(Input.GetMouseButtonDown(0))
         {
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
@@ -50,5 +33,10 @@ public class path : MonoBehaviour
                  Debug.Log ("Target Position: " + hit.collider.gameObject.transform.position);
             }
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        SceneManager.LoadScene(1); 
     }
 }
